b085f7a605328f75b85f9b763ac8b170e728bcae
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-03-18 18:55:03 +0530

    fix: use non-www domain as primary

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://www.sidvishnoi.com',
+	site: 'https://sidvishnoi.com',
 	integrations: [sitemap()],
 	output: 'static',
 	build: {
