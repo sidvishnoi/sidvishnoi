9c066077a87419ef81be571f973d1213b99db87c
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-03-18 17:30:13 +0530

    chore: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig, passthroughImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://www.sidvishnoi.com',
+	integrations: [sitemap()],
+	output: 'static',
+	adapter: cloudflare({
+		runtime: {
+			mode: 'local',
+			type: 'pages',
+			bindings: {},
+		},
+	}),
+	build: {
+		inlineStylesheets: 'always',
+	},
+	image: {
+		service: passthroughImageService(),
+	},
+});
