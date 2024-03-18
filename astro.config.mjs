ae137fd8dc4e6b1554e46c460dcd43e74dcd88d7
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-03-18 17:35:21 +0530

    chore: don't need cloudflare adapter, static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
 	site: 'https://www.sidvishnoi.com',
 	integrations: [sitemap()],
 	output: 'static',
-	adapter: cloudflare({
-		runtime: {
-			mode: 'local',
-			type: 'pages',
-			bindings: {},
-		},
-	}),
 	build: {
 		inlineStylesheets: 'always',
 	},
